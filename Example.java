d265a13dbdee7b209aa22cf92980869a248aaf36
Shreya4net <pujakumari.civil@gmail.com>
2016-12-26 16:25:58 +0530

    java session3 assignment 3
    java session3 assignment 3

---
+++ Example.java
@@ -0,0 +1,18 @@
+package session3;
+
+public class Example {
+int x;int y;
+String name ;
+public static void main(String args[]){
+	example pnt= new example();
+	System.out.println("pnt is" +pnt.name+""+pnt.x+""+pnt.y);
+}
+}
+
+
+//correct answer 
+// (b)
+//Exception in thread "main" java.lang.Error: Unresolved compilation problems: 
+//example cannot be resolved to a type
+//example cannot be resolved to a type
+
